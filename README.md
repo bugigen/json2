f6aae98fed5d615114472c6aba699a62b866b1d7
elgrenka <bugigen@yandex.ru>
2022-09-08 19:48:52 +0400

    add second fetch

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # json
 Link to my json: https://run.mocky.io/v3/adf501fe-5592-4766-8a10-0bc24f328807
 
-Link to my website: https://bugigen.github.io/js_1/
+Link to bank json: https://random-data-api.com/api/v2/banks
+
+Link to my website: https://bugigen.github.io/json2/
--- script.js
+++ script.js
@@ -1,32 +1,42 @@
 let content = document.querySelector("#content");
 let btn = document.querySelector("#btn");
 
-const url = "https://run.mocky.io/v3/adf501fe-5592-4766-8a10-0bc24f328807";
+const urls = [
+  "https://run.mocky.io/v3/adf501fe-5592-4766-8a10-0bc24f328807",
+  "https://random-data-api.com/api/v2/banks"
+]
 
-let getJson = async () => {
+let getJsons = async () => {
   try {
-    const response = await fetch(url);
+    const response = await Promise.all(urls.map(url => fetch(url)));
     if (response.ok === "false") {
       const message = `An error has occured: ${response.status}`;
       throw new Error(message);
     }
-    const answer = await response.json();
-    for (let prod in answer) {
-      content.innerHTML += `<b>${prod}:</b> <br>`;
-      for (let item = 0; item < answer[prod].length; item++) {
-        content.innerHTML += `&nbsp; <b>Name:</b> ${answer[prod][item].name},
-          <b>price:</b> ${answer[prod][item].price} руб <br>`
+    const answer = await Promise.all(response.map(res => res.json()));
+
+    content.innerHTML += '<h3><u>First response</u></h3><br>';
+
+    for (let prod in answer[0]) {
+      for (let item = 0; item < answer[0][prod].length; item++) {
+        content.innerHTML += `<b>Name:</b> ${answer[0][prod][item].name},
+        <b>price:</b> ${answer[0][prod][item].price} руб <br>`
       }
       content.innerHTML += "<br>";
     }
-    btn.removeEventListener("click", getJson);
+
+    content.innerHTML += '<br><h3><u>Second response</u></h3><br>';
+    content.innerHTML += `<b>Account number:</b> ${answer[1].account_number} <br>`
+    content.innerHTML += `<b>Bank name:</b> ${answer[1].bank_name} <br>`
+
+    btn.removeEventListener("click", getJsons);
   } catch (e) {
     console.log(e);
   }
 }
 
-// getJson().catch(error => error.message);
+// getJsons().catch(error => error.message);
 
-btn.addEventListener("click", getJson);
+btn.addEventListener("click", getJsons);
 
 
